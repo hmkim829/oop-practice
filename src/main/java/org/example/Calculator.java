43a0086fda409660b912b64a556c5b39fd715fe5
hmkim829 <hmkim829@colosseum.kr>
2023-02-22 13:30:34 +0900

    사칙연산 계산기 중간 커밋

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Calculator {
+    public static int calculate(int operand1, String operator, int operand2) {
+        return ArithmeitcOperator.calculate(operand1, operator, operand2);
+    }
+}
